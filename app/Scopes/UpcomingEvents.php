c0fda7d0c041ec1d135b3b518ec32690a81806a4
Marcel Stadelmann <m@marceli.to>
2024-03-04 22:02:56 +0100

    wip

---
+++ app/Scopes/UpcomingEvents.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Scopes;
+use Statamic\Query\Scopes\Scope;
+
+class UpcomingEvents extends Scope
+{
+  /**
+   * Apply the scope.
+   *
+   * @param \Statamic\Query\Builder $query
+   * @param array $values
+   * @return void
+   */
+  public function apply($query, $values)
+  {
+    $query->where('event_date', '>=', now());
+  }
+}
